a579007cb1e2eaeda1cddfa0228a37d64fdafe5f
SaMo-Hub <sacha.moricet@gmail.com>
2026-03-19 16:56:23 +0100

    aa

--- raw-studio/prisma/migrations/20260124125311_add_password_name_description/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "AccessKey" ADD COLUMN "description" TEXT;
-ALTER TABLE "AccessKey" ADD COLUMN "name" TEXT;
-ALTER TABLE "AccessKey" ADD COLUMN "password" TEXT;
--- raw-studio/prisma/migrations/20260208154359_add_client_date/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN "client" TEXT;
-ALTER TABLE "Project" ADD COLUMN "projectDate" DATETIME;
